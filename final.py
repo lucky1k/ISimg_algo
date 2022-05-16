0769264e79e8e05e2a85b95a0a5a9db9adaee2f0
lucky1k <101740340+lucky1k@users.noreply.github.com>
2022-05-16 10:07:25 +0530

    Add files via upload

---
+++ final.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+img=Image.open(r"r_test.jpg")
+img =img.convert('RGB')
+px=img.load()
+
+new_img=[]
+
+x,y=39,39
+
+print(px[x,y])
+(r,g,b)=px[x,y]
+print("Pixel at",x,"",y," - Red: {}, Green: {}, Blue: {}".format(r,g,b))
+
+new_img.append((104,105,105))
+
+print("##################")
+
+if px[x, y] == (r,g,b):
+    px[x, y] = (104, 105, 105)
+
+img.save("f_test.jpg")
+print(px[x,y])
+print("Pixel at",x,"",y," - Red: {}, Green: {}, Blue: {}".format(r,g,b))
+
+
+
+
+img.show()
