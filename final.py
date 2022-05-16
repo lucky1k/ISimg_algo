fae92479e3b50e349dacff42fd2c88aa6bd7d7fd
lucky1k <101740340+lucky1k@users.noreply.github.com>
2022-05-16 10:23:03 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-img=Image.open(r"r_test.jpg")
+img=Image.open(r"rplace.jpg")
 img =img.convert('RGB')
 px=img.load()
 
@@ -19,7 +19,7 @@ print("##################")
 if px[x, y] == (r,g,b):
     px[x, y] = (104, 105, 105)
 
-img.save("f_test.jpg")
+img.save("f_rplace.jpg")
 print(px[x,y])
 print("Pixel at",x,"",y," - Red: {}, Green: {}, Blue: {}".format(r,g,b))
 
